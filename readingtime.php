c3a2bdd319ed7427035dbd26a33c88f3d6aab47a
Roy Lodder <Roy@Roylodder.com>
2012-04-10 17:23:31 +0300

    Fixed some tabs.

--- readingtime.php
+++ readingtime.php
@@ -2,8 +2,8 @@
 
 	$content	= $page->text; 
 	$words		= str_word_count(strip_tags($content));
-	$m 			= floor($words / 200);
-	$s 			= floor($words % 200 / (200 / 60));
+	$m 		= floor($words / 200);
+	$s 		= floor($words % 200 / (200 / 60));
 	//$estimate 	= $m . ' ' . ($m == 0 ? 'minuten' : '') . ($m == 1 ? 'minuut' : '') . ($m >= 2 ? 'minuten' : '') . ', ' . $s . ' seconde' . ($s == 1 ? '' : 'n');
 	$estimate 	= $m . ' minute' . ($m == 1 ? '' : 's') . ', ' . $s . ' second' . ($s == 1 ? '' : 's');
 
